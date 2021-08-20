5f149bb522f787042c6e9bf8dd842309f95ad837
DL <damir96.lukyanenko@gmail.com>
2021-08-20 13:08:05 +0300

    make short & clear scroll up Jquery solution

--- js/script.js
+++ js/script.js
@@ -1,36 +1,22 @@
-// Select all links with hashes
-$('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .on('click', function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.trigger('focus');
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.trigger('focus'); // Set focus again
-          };
-        });
-      }
-    }
-  });
+// элемент, по которому кликаем
+let clicked = $(".scroll-up");
+
+// находим, как далеко прокручен html 
+// (ползунок в браузере справа)
+let htmlScrollTop = $('html').scrollTop();
+
+// и выводим результат
+console.log(htmlScrollTop);
+
+// вызываем клик с помощью jquery
+clicked.on("click", function () {
+
+  // обращаемся к html и находим его верхнюю точку
+	$("html").animate({
+    scrollTop: 0,
+    // длинна анимации
+  }, 2000);
+  
+  console.log(htmlScrollTop);
+});
+

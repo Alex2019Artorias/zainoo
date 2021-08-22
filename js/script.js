84b288e13bd8f2336544798f7acae1e340a033cb
Alex2019Artorias <2019.alex.kim@gmail.com>
2021-08-22 10:47:21 +0300

    scroll top animated

--- js/script.js
+++ js/script.js
@@ -1,22 +1,33 @@
-// элемент, по которому кликаем
-let clicked = $(".scroll-up");
+// // элемент, по которому кликаем
+// let clicked = $(".scroll-up");
 
-// находим, как далеко прокручен html 
-// (ползунок в браузере справа)
-let htmlScrollTop = $('html').scrollTop();
+// // находим, как далеко прокручен html 
+// // (ползунок в браузере справа)
+// let htmlScrollTop = $('html').scrollTop();
 
-// и выводим результат
-console.log(htmlScrollTop);
+// // и выводим результат
+// console.log(htmlScrollTop);
 
-// вызываем клик с помощью jquery
-clicked.on("click", function () {
+// // вызываем клик с помощью jquery
+// clicked.on("click", function () {
 
-  // обращаемся к html и находим его верхнюю точку
-	$("html").animate({
-    scrollTop: 0,
-    // длинна анимации
-  }, 2000);
+//   // обращаемся к html и находим его верхнюю точку
+// 	$("html").animate({
+//     scrollTop: 0,
+//     // длинна анимации
+//   }, 2000);
   
-  console.log(htmlScrollTop);
-});
+//   console.log(htmlScrollTop);
+// });
 
+
+
+// сделать так, что бы можно было нажать на scroll up
+// подьём ввверз
+
+
+  
+$('#up').click(function() {
+  $('html').animate({scrollTop: 0},500); //время прокрутки
+  return false;
+})
